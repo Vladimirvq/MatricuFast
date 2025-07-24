cc837587a89ff6d82ce5443639cf7fcb445a1657
AllenVillalobos <allenvs06@gmail.com>
2025-07-23 19:43:57 -0600

    Creacion de carpetas y modelos

---
+++ DAL/EstudianteDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatricuFast.DAO
+{
+    public class EstudianteDAL
+    {
+        private readonly string connectionString;
+
+        public EstudianteDAL(string connectionString)
+        {
+            this.connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConexionDB"].ConnectionString;
+        }
+
+        public 
+    }
+}
