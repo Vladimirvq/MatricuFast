5353650ab228898ba21c113f1e6f6aec0b32c01d
AllenVillalobos <allenvs06@gmail.com>
2025-07-23 19:53:05 -0600

    Creacion de agregar estudiantes y estudiantes

--- Models/Estudiante.cs
+++ Models/Estudiante.cs
@@ -8,14 +8,14 @@ namespace MatricuFast.Models
     public class Estudiante
     {
 
-    public int EstudianteID { get; set; }
-    public string Nombre { get; set; }
-    public string Apellido { get; set; }
-    public string Email { get; set; }
-    public DateTime FechaNacimiento { get; set; }
-    public string AdicionadoPor { get; set; }
-    public DateTime FechaAdicion { get; set; }
-    public string ModificadoPor { get; set; }
-    public DateTime FechaModificacion { get; set; }
+        public int EstudianteID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string AdicionadoPor { get; set; }
+        public DateTime FechaAdicion { get; set; }
+        public string ModificadoPor { get; set; }
+        public DateTime FechaModificacion { get; set; }
     }
 }
